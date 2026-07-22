0fb53326ae745058046ba2b424415eea6447eb36
Maxime <maxime.bulot@gmail.com>
2026-07-22 18:33:40 +0200

    Ajout gestion mise à jour automatique PWA

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,52 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import 'leaflet/dist/leaflet.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import "leaflet/dist/leaflet.css";
+
+import App from "./App.tsx";
+
+import { registerSW } from "virtual:pwa-register";
+
+
+// Gestion mise à jour PWA
+const updateSW = registerSW({
+
+  onNeedRefresh() {
+
+    if (
+      confirm(
+        "Une nouvelle version de PATROUILLE-PARCS est disponible. Mettre à jour ?"
+      )
+    ) {
+
+      updateSW(true);
+
+    }
+
+  },
+
+
+  onOfflineReady() {
+
+    console.log(
+      "PATROUILLE-PARCS disponible hors connexion"
+    );
+
+  },
+
+});
+
+
+
+createRoot(
+  document.getElementById("root")!
+).render(
+
   <StrictMode>
+
     <App />
-  </StrictMode>,
-)
+
+  </StrictMode>
+
+);
